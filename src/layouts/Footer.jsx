14344b268579cb0e8c04dc997b373dc39645dd23
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-04-02 18:57:46 +0530

    Add social links

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,21 +1,28 @@
 import GridContainer from "../components/GridContainer";
 import { Github, Linkedin, Instagram } from "lucide-react";
+
 const Footer = () => {
+    const socialLinks = [
+        { icon: <Github />, to: "https://github.com/gihanchamila" },
+        { icon: <Linkedin />, to: "https://www.linkedin.com/in/algihanchamila" },
+        { icon: <Instagram />, to: "https://www.instagram.com/zander_xz" },
+    ];
+
     return (
-    <GridContainer>
-        <footer className="text-gray-800 font-primary lg:col-start-2 lg:col-end-12 col-span-4 relative flex flex-col items-center py-4">
-            <h2 className="text-lg font-semibold">Gihan Chamila</h2>
-            <p className="text-sm text-gray-400">algihanchamila@gmail.com</p>
-            <div className="flex gap-4 mt-2">
-                <Github className="icon" />
-                <Linkedin className="icon" />
-                <Instagram className="icon" />
-            </div>
-        </footer>
-    </GridContainer>
-      
+        <GridContainer>
+            <footer className="text-gray-800 font-primary lg:col-start-2 lg:col-end-12 col-span-4 relative flex flex-col items-center py-4">
+                <h2 className="text-lg font-semibold">Gihan Chamila</h2>
+                <p className="text-sm text-gray-400">algihanchamila@gmail.com</p>
+                <div className="flex gap-4 mt-2">
+                    {socialLinks.map((link, index) => (
+                        <a key={index} href={link.to} target="_blank" rel="noopener noreferrer" className="hover:text-gray-500 transition">
+                            {link.icon}
+                        </a>
+                    ))}
+                </div>
+            </footer>
+        </GridContainer>
     );
-  };
-  
-  export default Footer;
-  
+};
+
+export default Footer;
