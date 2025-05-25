b16a39f3362a93c2e49dc8d7d2e025d8df3f2329
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-05-25 20:02:25 +0530

    refactor: remove unused imports and variables in DesktopMenu component

--- src/components/DesktopMenu.jsx
+++ src/components/DesktopMenu.jsx
@@ -1,14 +1,10 @@
-
 import GridContainer from './GridContainer'
 import NavLinks from './NavLinks'
-import Button from './utils/Button'
 import ResumeDownload from './utils/ResumeDownload'
 import { useAuth } from '../context/AuthContext'
 import { LogOut } from 'lucide-react'
-import { useLocation } from 'react-router-dom'
 
 const DesktopMenu = ({navLinks}) => {
-    const location = useLocation()
     const {admin, signOut} = useAuth()
   return (
         <GridContainer>
