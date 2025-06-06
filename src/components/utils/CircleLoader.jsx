04056537bfaebe57e25ea604ca28ce35f7f08308
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-06-06 16:58:42 +0530

    seperate circle of loading

--- src/components/utils/CircleLoader.jsx
+++ src/components/utils/CircleLoader.jsx
@@ -1,13 +1,17 @@
 // CircleLoader.jsx
 import { motion } from 'framer-motion';
 
-const CircleLoader = () => (
-  <div className="fixed inset-0 flex justify-center items-center bg-white z-50">
+export const Circle = () => (
     <motion.div
-      className="w-5 h-5 border-2 border-t-transparent border-sky-500 rounded-full mr-3"
+      className="w- h-5 border-2 border-t-transparent border-sky-500 rounded-full mr-3"
       animate={{ rotate: 360 }}
       transition={{ repeat: Infinity, ease: "linear", duration: 1 }}
     />
+)
+
+const CircleLoader = () => (
+  <div className="fixed inset-0 flex justify-center items-center bg-white z-50">
+    <Circle />
     <p className="text-sky-500 text-lg font-medium font-primary">Just a moment</p>
   </div>
 );
