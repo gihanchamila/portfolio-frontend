c0033e8d15fdfb433b23051da3f5d7d8e3f77e5d
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-06-07 18:59:10 +0530

    code refractor

--- src/components/utils/CircleLoader.jsx
+++ src/components/utils/CircleLoader.jsx
@@ -2,11 +2,11 @@
 import { motion } from 'framer-motion';
 
 export const Circle = () => (
-    <motion.div
-      className="w-5 h-5 border-2 border-t-transparent border-sky-500 rounded-full mr-3"
-      animate={{ rotate: 360 }}
-      transition={{ repeat: Infinity, ease: "linear", duration: 1 }}
-    />
+  <motion.div
+    className="w-5 h-5 border-2 border-t-transparent border-sky-500 rounded-full mr-3"
+    animate={{ rotate: 360 }}
+    transition={{ repeat: Infinity, ease: "linear", duration: 1 }}
+  />
 )
 
 const CircleLoader = () => (
