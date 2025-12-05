6e7ef12324fa015f049469b172710e73486eac82
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-12-05 12:20:18 +0530

    refactor : remove unused code

--- src/components/utils/LoginForm.jsx
+++ src/components/utils/LoginForm.jsx
@@ -13,15 +13,6 @@ const LoginForm = () => {
   const { toast } = useToast();
   const { signIn } = useAuth();
 
-  const containerVariant = {
-    hidden: {},
-    visible: {
-      transition: {
-        staggerChildren: 0.2
-      }
-    }
-  };
-
   const childVariant = {
     hidden: { opacity: 0, y: 30, scale: 0.95 },
     visible: { opacity: 1, y: 0, scale: 1, transition: { duration: 0.6, ease: 'easeOut' } }
