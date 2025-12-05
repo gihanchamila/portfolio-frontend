9da20daadad767692901a20edae389a9bba313de
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-12-05 12:18:53 +0530

    refactor : change button text when email is submitting,

--- src/components/utils/ContactForm.jsx
+++ src/components/utils/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import DOMPurify from 'dompurify';
 import * as Yup from 'yup';
@@ -11,24 +11,21 @@ import debounce from 'lodash.debounce';
 import { useToast } from '../../context/ToastContext';
 
 const ContactForm = () => {
-  const fullnameRef = useRef();
-  const emailRef = useRef();
-  const messageRef = useRef();
   const { toast } = useToast();
   const [isEmailVerified, setIsEmailVerified] = useState(false);
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   const [lastCodeSentTime, setLastCodeSentTime] = useState(null);
-  const [verificationCode, setVerificationCode] = useState(null);
+  const [verificationCode, setVerificationCode] = useState('');
   const [count, setCount] = useState(null);
+
   useDisableBackgroundScroll(isPopupOpen);
 
   useEffect(() => {
-    let interval;
-    if (isEmailVerified && count > 0) {
-      interval = setInterval(() => {
-        setCount(prev => prev - 1);
-      }, 60000);
-    }
+    if (!isEmailVerified || !count || count <= 0) return;
+
+    const interval = setInterval(() => {
+      setCount(prev => (prev > 0 ? prev - 1 : 0));
+    }, 60000);
 
     return () => clearInterval(interval);
   }, [isEmailVerified, count]);
@@ -43,208 +40,198 @@ const ContactForm = () => {
       .min(10, 'Message must be at least 10 characters long')
   });
 
-  const handleVerifyEmail = debounce(async (email, name) => {
-    const now = new Date().getTime();
-    const cooldownPeriod = 5 * 60 * 1000;
+  const handleAxiosError = error => {
+    const msg = error?.response?.data?.message || 'An error occurred';
+    toast(msg, 'error', 3000, 'bottom-right');
+  };
 
-    if (lastCodeSentTime && now - lastCodeSentTime < cooldownPeriod) {
-      const minutesLeft = Math.ceil((cooldownPeriod - (now - lastCodeSentTime)) / 60000);
-      toast(`Please wait ${minutesLeft} minute(s) before requesting another code.`, 'error', 3000);
-      return;
-    }
+  const verifyEmail = useCallback(
+    debounce(async (email, name) => {
+      try {
+        const now = Date.now();
+        const cooldown = 5 * 60 * 1000;
 
+        if (lastCodeSentTime && now - lastCodeSentTime < cooldown) {
+          const minutesLeft = Math.ceil((cooldown - (now - lastCodeSentTime)) / 60000);
+          toast(`Wait ${minutesLeft} minute(s) before requesting another code.`, 'error');
+          return;
+        }
+
+        setIsPopupOpen(true);
+        const res = await axios.post('/user/send-verification-code', { email, name });
+
+        toast(res.data.message, 'success', 3000, 'bottom-right');
+        setLastCodeSentTime(now);
+        setCount(10);
+      } catch (error) {
+        handleAxiosError(error);
+      }
+    }, 500),
+    [lastCodeSentTime]
+  );
+
+  const handleVerifyCode = async (email, code) => {
     try {
-      setIsPopupOpen(true);
-      const response = await axios.post('/user/send-verification-code', { email, name });
-      const data = response.data;
-      toast(data.message, 'success', 3000, 'bottom-right');
-      setCount(10);
-    } catch (error) {
-      const response = error.response;
-      const data = response?.data?.message || 'An error occurred';
-      toast(data, 'bottom-right');
-    }
-  }, 500);
+      const res = await axios.post('/user/verify-user', { code, email });
+      toast(res.data.message, 'success', 3000, 'bottom-right');
 
-  const handleVerifyCode = async (email, verificationCode) => {
-    try {
-      const response = await axios.post('/user/verify-user', {
-        code: verificationCode,
-        email
-      });
-      const data = response.data;
-      if (data.success == true) {
+      if (res.data.success === true) {
         setIsEmailVerified(true);
         setIsPopupOpen(false);
       }
-      toast(data.message, 'success', 3000, 'bottom-right');
     } catch (error) {
-      const response = error.response;
-      const data = response.data;
-      console.error(data.message);
-      toast(data.message, 'error', 3000, 'bottom-right');
+      handleAxiosError(error);
+    }
+  };
+
+  const handleEmailChange = (newEmail, verifiedEmail) => {
+    if (verifiedEmail && newEmail !== verifiedEmail) {
+      setIsEmailVerified(false);
+      setVerificationCode('');
+      setLastCodeSentTime(null);
+      setCount(null);
     }
   };
 
   const handleClosePopup = () => {
     setIsPopupOpen(false);
-    setVerificationCode(null);
-  };
-
-  const handleInputChange = e => {
-    const { value } = e.target;
-    setVerificationCode(value);
-  };
-
-  const handleChangeEmail = () => {
-    setIsEmailVerified(false);
-    setVerificationCode(null);
-    setLastCodeSentTime(null);
-    setCount(null);
-  };
-
-  const handleKeyDown = (e, value, nextRef) => {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      if (value.trim() !== '' && nextRef?.current) {
-        nextRef.current.focus();
-      }
-    }
+    setVerificationCode('');
   };
 
   return (
-    <>
-      <Formik
-        initialValues={{ fullName: '', email: '', message: '' }}
-        validationSchema={validationSchema}
-        onSubmit={async (values, { resetForm }) => {
-          try {
-            const contactData = {
-              name: DOMPurify.sanitize(values.fullName),
-              email: DOMPurify.sanitize(values.email),
-              message: DOMPurify.sanitize(values.message)
-            };
-            const response = await axios.post('/connect/make-connection', contactData);
-            const data = response.data;
-            toast(data.message);
-            resetForm();
-          } catch (error) {
-            const response = error.response;
-            const data = response.data.message;
-            toast(data);
-          }
-        }}
-      >
-        {({ isSubmitting, values }) => (
-          <div className="sm:w-full lg:w-1/2">
-            <Form>
-              <div className="mb-4">
-                <label htmlFor="fullName" className="formLable">
-                  Full Name <Asterisk className="inline-block align-super text-red-500" size={10} />
-                </label>
-                <Field
+    <Formik
+      initialValues={{ fullName: '', email: '', message: '' }}
+      validationSchema={validationSchema}
+      onSubmit={async (values, { setFieldValue }) => {
+        try {
+          const payload = {
+            name: DOMPurify.sanitize(values.fullName),
+            email: DOMPurify.sanitize(values.email),
+            message: DOMPurify.sanitize(values.message)
+          };
+
+          const res = await axios.post('/connect/make-connection', payload);
+          toast(res.data.message, 'success');
+          setFieldValue('message', '');
+          setCount(null);
+        } catch (error) {
+          handleAxiosError(error);
+        }
+      }}
+    >
+      {({ values, setFieldValue, isSubmitting }) => (
+        <div className="sm:w-full lg:w-1/2">
+          <Form>
+            <div className="mb-4">
+              <label htmlFor="fullName" className="formLable">
+                Full Name <Asterisk size={10} className="inline-block align-super text-red-500" />
+              </label>
+              <Field
+                type="text"
+                name="fullName"
+                className="formInput"
+                placeholder="e.g., Jane Doe"
+              />
+              <ErrorMessage name="fullName" component="div" className="formError" />
+            </div>
+
+            <div className="relative mb-4">
+              <label htmlFor="email" className="formLable">
+                Email <Asterisk size={10} className="inline-block align-super text-red-500" />
+              </label>
+
+              <Field
+                type="email"
+                name="email"
+                disabled={isEmailVerified}
+                className="formInput"
+                placeholder="e.g., jane.doe@example.com"
+                onChange={e => {
+                  setFieldValue('email', e.target.value);
+                  handleEmailChange(e.target.value, values.email);
+                }}
+              />
+
+              <ErrorMessage name="email" component="div" className="formError" />
+              {!isEmailVerified && values.email && (
+                <p className="formError">You must verify your email before sending a message.</p>
+              )}
+
+              <div className="absolute -top-2 right-0 mt-2">
+                {!isEmailVerified && values.email && (
+                  <span
+                    onClick={() => verifyEmail(values.email, values.fullName)}
+                    className="font-primary cursor-pointer text-sm text-sky-500 hover:underline"
+                  >
+                    Verify Email
+                  </span>
+                )}
+
+                {isEmailVerified && (
+                  <span
+                    onClick={() => {
+                      setIsEmailVerified(false);
+                      setCount(null);
+                    }}
+                    className="font-primary text-sm text-red-500 cursor-pointer hover:underline"
+                  >
+                    Change Email
+                  </span>
+                )}
+              </div>
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="message" className="formLable">
+                Message <Asterisk size={10} className="inline-block align-super text-red-500" />
+              </label>
+
+              <Field
+                as="textarea"
+                name="message"
+                rows="5"
+                className="formInput caret-gray-200"
+                placeholder="Tell me about your project…"
+              />
+              <ErrorMessage name="message" component="div" className="formError" />
+            </div>
+
+            <div className="flex justify-end">
+              <Button variant="primary" type="submit" disabled={!isEmailVerified || isSubmitting}>
+                {isSubmitting ? 'Sending...' : isEmailVerified ? 'Send Message' : 'Verify Email'}
+              </Button>
+            </div>
+
+            <Popup isOpen={isPopupOpen} onClose={handleClosePopup}>
+              <h2 className="cardTitle pb-5">Verify Your Email</h2>
+              <p className="font-primary text-gray-800 dark:text-white">
+                A verification code has been sent to your email. Please enter the code:
+              </p>
+
+              <div className="flex w-full justify-center py-6">
+                <input
                   type="text"
-                  id="fullName"
-                  name="fullName"
-                  className="formInput"
-                  placeholder="e.g., Jane Doe"
-                  innerRef={fullnameRef}
-                  onKeyDown={e => handleKeyDown(e, e.target.value, emailRef)}
+                  maxLength="6"
+                  value={verificationCode}
+                  onChange={e => setVerificationCode(e.target.value)}
+                  className="rounded-lg border-2 border-gray-500 py-2 pl-4 text-lg sm:w-full"
+                  placeholder="Enter verification code"
                 />
-                <ErrorMessage name="fullName" component="div" className="formError" />
               </div>
 
-              <div className="relative mb-4">
-                <label htmlFor="email" className="formLable">
-                  Email <Asterisk className="inline-block align-super text-red-500" size={10} />
-                </label>
-                <Field
-                  type="email"
-                  id="email"
-                  disabled={isEmailVerified}
-                  name="email"
-                  className="formInput"
-                  placeholder="e.g., jane.doe@example.com"
-                  innerRef={emailRef}
-                />
-                <ErrorMessage name="email" component="div" className="formError" />
-
-                <div className="absolute -top-2 right-0 mt-2">
-                  {!isEmailVerified && values.email && (
-                    <span
-                      onClick={() => handleVerifyEmail(values.email, values.fullName)}
-                      className="font-primary cursor-pointer text-sm text-sky-500 hover:underline"
-                    >
-                      Verify Email
-                    </span>
-                  )}
-
-                  {isEmailVerified && (
-                    <span
-                      onClick={count === 0 ? handleChangeEmail : null}
-                      className={`font-primary cursor-pointer text-sm hover:underline ${
-                        count > 0 ? 'cursor-not-allowed text-gray-400' : 'text-red-500'
-                      }`}
-                    >
-                      {count > 0 ? `Wait ${count} minutes to Change Email` : 'Change Email'}
-                    </span>
-                  )}
-                </div>
-              </div>
-
-              <div className="mb-4">
-                <label htmlFor="message" className="formLable">
-                  Message <Asterisk className="inline-block align-super text-red-500" size={10} />
-                </label>
-                <Field
-                  as="textarea"
-                  id="message"
-                  name="message"
-                  className="formInput caret-gray-200"
-                  placeholder="Tell me a bit about your project or how I can help…"
-                  rows="5"
-                  innerRef={messageRef}
-                />
-                <ErrorMessage name="message" component="div" className="formError" />
-              </div>
-
-              <div className="flex justify-end">
-                <Button variant="primary" type="submit" disabled={isSubmitting || !isEmailVerified}>
-                  {isSubmitting ? 'Submitting...' : 'Submit'}
-                </Button>
-              </div>
-
-              <Popup isOpen={isPopupOpen} onClose={handleClosePopup}>
-                <h2 className="cardTitle pb-5 drop-shadow-none">Verify Your Email</h2>
-                <p className="font-primary text-gray-800 dark:text-white">
-                  A verification code has been sent to your email. Please enter the code below:
-                </p>
-
-                <div className="flex w-full justify-center py-6">
-                  <input
-                    type="text"
-                    maxLength="6"
-                    value={verificationCode}
-                    onChange={e => handleInputChange(e)}
-                    className="rounded-lg border-2 border-gray-500 py-2 pl-4 text-left text-lg sm:w-full"
-                    placeholder="Enter verification code"
-                  />
-                </div>
-
-                <Button
-                  onClick={() => handleVerifyCode(values.email, verificationCode)}
-                  disabled={verificationCode?.length !== 6}
-                  variant="primary"
-                  className="mt-2"
-                >
-                  Verify Code
-                </Button>
-              </Popup>
-            </Form>
-          </div>
-        )}
-      </Formik>
-    </>
+              <Button
+                onClick={() => handleVerifyCode(values.email, verificationCode)}
+                disabled={verificationCode.length !== 6}
+                variant="primary"
+                className="mt-2"
+              >
+                Verify Code
+              </Button>
+            </Popup>
+          </Form>
+        </div>
+      )}
+    </Formik>
   );
 };
 
