2cc0af07816600208a251d6d25f8fe496e603083
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-12-07 14:32:43 +0530

    feat : add align property to adjust text alignment

--- src/components/utils/Title.jsx
+++ src/components/utils/Title.jsx
@@ -1,9 +1,22 @@
 import Reveal from './Reveal';
+import clsx from 'clsx';
 
-const Title = ({ text, className }) => {
+const Title = ({ text, align = 'left', className }) => {
   return (
     <Reveal>
-      <h2 className={className || 'xs:text-3xl font-primary font-bold sm:text-4xl pb-8'}>{text}</h2>
+      <h2
+        className={clsx(
+          'xs:text-3xl font-primary font-bold sm:text-4xl pb-8',
+          {
+            'text-left': align === 'left',
+            'text-center': align === 'center',
+            'text-right': align === 'right'
+          },
+          className
+        )}
+      >
+        {text}
+      </h2>
     </Reveal>
   );
 };
