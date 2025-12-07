2a4eaff357735c177d735a0606049fc3862bce15
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-12-07 13:05:58 +0530

    feat : add onItemClick prop to the component

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,14 +1,14 @@
 import NavItem from './NavItem';
 import useScrollSpy from '../hooks/useScrollSpy';
 
-const NavLinks = ({ links }) => {
+const NavLinks = ({ links, onItemClick }) => {
   const sectionIds = links.filter(link => link.to.startsWith('#')).map(link => link.to);
 
   const activeHash = useScrollSpy(sectionIds, 150); // 150 px from top
   return (
     <div className="font-primary xs:text-base flex w-full flex-col gap-y-10 font-bold md:text-xl lg:flex-row lg:gap-5">
       {links.map(link => (
-        <NavItem key={link.to} link={link} activeHash={activeHash} />
+        <NavItem key={link.to} link={link} activeHash={activeHash} onClick={onItemClick} />
       ))}
     </div>
   );
