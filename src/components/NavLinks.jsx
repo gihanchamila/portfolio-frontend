0236899a8546758de9de5c75919c41c5d34eec8c
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-12-07 13:11:56 +0530

    refactor : code refactor

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -4,7 +4,7 @@ import useScrollSpy from '../hooks/useScrollSpy';
 const NavLinks = ({ links, onItemClick }) => {
   const sectionIds = links.filter(link => link.to.startsWith('#')).map(link => link.to);
 
-  const activeHash = useScrollSpy(sectionIds, 150); // 150 px from top
+  const activeHash = useScrollSpy(sectionIds, 150);
   return (
     <div className="font-primary xs:text-base flex w-full flex-col gap-y-10 font-bold md:text-xl lg:flex-row lg:gap-5">
       {links.map(link => (
