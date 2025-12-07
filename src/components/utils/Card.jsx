71006eb6aeefa49547dd1dd672c86a2806c51e86
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-12-07 19:27:29 +0530

    style : change card size for larger screens

--- src/components/utils/Card.jsx
+++ src/components/utils/Card.jsx
@@ -13,7 +13,7 @@ const ProjectCard = ({ projectName, description, imageUrl, github, live, project
   return (
     <div
       ref={ref}
-      className="xs:h-[25rem] group relative overflow-hidden rounded-2xl border border-gray-300 bg-white sm:h-[30rem] lg:h-[30rem] dark:border-none"
+      className="xs:h-[25rem] group relative overflow-hidden rounded-2xl border border-gray-300  sm:h-[32rem] dark:border-none"
     >
       <div className="relative h-full w-full overflow-hidden">
         <img
