a159b4a7f4b4474a85c7277b67205d5fec690791
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-12-07 13:13:22 +0530

    feat : add onClick event to close the mobile menu when navitem clicked

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -1,22 +1,24 @@
 import { NavLink, useLocation } from 'react-router-dom';
 
-const NavItem = ({ link, activeHash }) => {
+const NavItem = ({ link, activeHash, onClick }) => {
   const isHash = link.to.startsWith('#');
   const { hash } = useLocation();
 
   const handleClick = e => {
-    if (!isHash) return;
+    if (isHash) {
+      e.preventDefault();
+      const element = document.querySelector(link.to);
 
-    e.preventDefault();
-    const element = document.querySelector(link.to);
-
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
-      window.history.replaceState(null, '', link.to);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+        window.history.replaceState(null, '', link.to);
+      }
     }
+
+    if (onClick) onClick();
   };
 
-  const isActive = isHash ? activeHash === link.to : link.to === '/' && activeHash === ''; // FIXED
+  const isActive = isHash ? activeHash === link.to : link.to === '/' && activeHash === '';
 
   return isHash ? (
     <a
@@ -29,6 +31,7 @@ const NavItem = ({ link, activeHash }) => {
   ) : (
     <NavLink
       to={link.to}
+      onClick={onClick}
       className={({ isActive: routeActive }) =>
         routeActive && activeHash === '' ? 'text-sky-500 dark:text-sky-300' : 'navLink'
       }
