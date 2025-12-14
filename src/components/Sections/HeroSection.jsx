234ef8e5a10f6876a861616b54a5a2d29823e656
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-12-14 17:34:00 +0530

    feat : use aspect ratios for images

--- src/components/Sections/HeroSection.jsx
+++ src/components/Sections/HeroSection.jsx
@@ -60,7 +60,7 @@ const HeroSection = () => {
               onMouseEnter={() => setCursorVisible(true)}
               onMouseLeave={() => setCursorVisible(false)}
               alt="A personal headshot of Gihan Chamila, the developer."
-              className="xs:mb-6 xs:mt-5 h-[250px] w-[250px] rounded-2xl lg:mt-0"
+              className="xs:mb-6 xs:mt-5 h-[250px] w-[250px] aspect-square rounded-2xl lg:mt-0"
             />
           </div>
         </div>
@@ -105,7 +105,7 @@ const HeroSection = () => {
             onMouseEnter={() => setCursorVisible(true)}
             onMouseLeave={() => setCursorVisible(false)}
             alt="A personal headshot of Gihan Chamila, the developer."
-            className="xs:w-[200px] xs:h-[200px] xs:mt-5 rounded-2xl lg:mt-0 lg:mb-0 lg:h-[380px] lg:w-[400px] 2xl:h-[400px] 2xl:w-[400px]"
+            className="aspect-square xs:mt-5 rounded-2xl lg:mt-0 lg:mb-0 lg:h-[380px] lg:w-[400px] 2xl:h-[400px] 2xl:w-[400px]"
           />
         </div>
       </section>
